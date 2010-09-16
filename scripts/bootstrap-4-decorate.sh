1f177666d54fceb8a7831367e572ebeb5b5b61bf
YAMAGUCHI Kiyohiro <kiyoya@gmail.com>
2010-09-17 00:20:19 +0900

    iptables, sshd

--- scripts/bootstrap-4-decorate.sh
+++ scripts/bootstrap-4-decorate.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-sed -i \
-	-e 's|^USE="\([^"]\+\)"|USE="cjk \1 vim-syntax zsh-completion"|' \
-	/etc/make.conf
+emerge --update --deep world --quiet
 
-emerge screen sudo vim zsh --quiet
-sed -i \
-	-e 's|^EDITOR="\([^"]\+\)"|#EDITOR="\1"|' \
-	-e 's|^#EDITOR="/usr/bin/vim"|EDITOR="/usr/bin/vim"|' \
-	/etc/rc.conf
+emerge sudo --quiet
 sed -i \
 	-e 's|^# \(%wheel ALL=(ALL) ALL\)|\1|' \
 	/etc/sudoers
 
-emerge --update --deep --newuse world --quiet
+emerge iptables --quiet
+/etc/init.d/iptables start
+rc-update add iptables default
+iptables -F
+iptables -X
+iptables -Z
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT
+/etc/init.d/iptables save
+
+sed -i \
+	-e "s|^#ChallengeResponseAuthentication yes|ChallengeResponseAuthentication no|" \
+	/etc/ssh/sshd_config
+/etc/init.d/sshd restart
