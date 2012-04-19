67d4e8667cd97e0570b321ea929c6c8338ac9f79
mat2uken <ku@yasagure.jp>
2012-04-19 20:36:34 +0900

    comit

--- scripts/bootstrap-3-finalize.sh
+++ scripts/bootstrap-3-finalize.sh
@@ -3,18 +3,19 @@
 rm -f /stage3-*.tar.bz2
 rm -f /portage-latest.tar.bz2
 
-rm -f /swap.img
-fdisk /dev/sda <<EOM
+#rm -f /swap.img
+fdisk /dev/vda <<EOM
 t
-3
+2
 82
 w
 EOM
 mkswap /dev/sda3
 sed -i \
-	-e "s:#/dev/sda3:/dev/sda3:" \
+	-e "s:#/dev/vda3:/dev/vda3:" \
 	/etc/fstab
 
 rm -f /kernel-version.txt
 
 reboot
+
